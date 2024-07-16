e4715bf42e03295e215a5f95e7fded8dbcd5bafe
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-07-16 15:04:12 -0700

    save

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,37 @@ import {useState,useRef, useEffect} from "react";
 import { BgOverlay,StartScreen,Instructions,Categories,Game } from "@/components";
 import Template from "./template"
 import {motion} from "framer-motion"
+import { FaVolumeHigh } from "react-icons/fa6";
+import { FaVolumeMute } from "react-icons/fa";
+
+
+
+const AudioBtn = ({audioRef}:any)=>{
+  const [isPlaying,setIsPlaying] = useState(false);
+
+
+  return(
+  <button onClick={()=>{
+    console.log("btnClicked")
+    if(!isPlaying){
+    audioRef.current.play()
+    setIsPlaying(true);
+    }
+    else{
+      audioRef.current.pause()
+      setIsPlaying(false);
+
+    }
+
+  }} className="absolute z-1000 right-5 top-5 text-3xl">
+      {!isPlaying ? 
+      <FaVolumeMute color="white" />
+          :
+      <FaVolumeHigh color="white"/>
+    }
+    </button>
+  )
+}
 
 
 export default function Home() {
@@ -33,18 +64,9 @@ export default function Home() {
     playAudio();
   }
   return (
-    <motion.main onDoubleClick={()=>{
-      if(audioRef.current.paused){
-      audioRef.current.play()
-      }
-      else{
-        audioRef.current.pause()
-
-      }
-
-    }} initial={{translateY:'0%'}} animate={{translateY:0}} transition={{type:"spring"}} className="main-container">
+    <motion.main initial={{translateY:'0%'}} animate={{translateY:0}} transition={{type:"spring"}} className="main-container">
     {/* <main className="main-container"> */}
-    
+
       <BgOverlay/>
     
         <Template key={page}> 
@@ -57,6 +79,7 @@ export default function Home() {
        <audio ref={audioRef as any} loop={true} id="audioPlayer" className="w-100 absolute hidden" controls src="/gameaudio.mp3">
         Your browser does not support the audio element.
       </audio>
+      <AudioBtn audioRef={audioRef}/>
 
 
 
